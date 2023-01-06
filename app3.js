4419380d004052f60da79efd1c5b38487000a9a3
Simiyu Kuloba <hellocodepark@gmail.com>
2023-01-06 21:24:18 +0300

    add app3.js

---
+++ app3.js
@@ -0,0 +1,6 @@
+// EVENT-LISTENER-FOR-GET-QUOTE
+document.getElementById('order-form').addEventListener('submit', function(e){
+  alert('send quote');
+
+  e.preventDefault();
+})
--- app5.js
+++ app5.js
@@ -84,3 +84,4 @@ document.getElementById('contact-form').addEventListener('submit', function(e){
 
   e.preventDefault();
 })
+
